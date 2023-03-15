69b90b30b6e8a4062a0335c4ffcbcd45423d6b4a
Roshan panda <roshanpanda666@gmail.com>
2023-03-15 20:12:34 +0530

    commiting

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 
 import { useState } from 'react'
+import { useEffect } from 'react'
+import Confetti from 'react-confetti'
+import axios from 'axios';
+
 import './App.css'
 
 function App() {
+
+
 const[befor,after]=useState("Hello vite")
 const[noclick,click]=useState(0)
 const[bool,nullbool]=useState(true)
@@ -17,16 +23,32 @@ let clickPlus=()=>{
 click(noclick+1)
 nullbool(!bool)
 }
+ 
+
 
   return (
+    <>
+    
     <div className="App">
       
       <input type="text" placeholder='type any thing' onChange={changetxt} />
       <h1>{befor}</h1>
       <h1>{noclick}</h1>
       <button onClick={clickPlus}>incress</button>
-      {bool && <p>boolean</p>}    
+      {bool && <p>boolean</p>}  
+
+      
+      
+  
     </div>
+    <div className="con">
+    <Confetti
+    width={300}
+    height={300}
+    />
+    </div>
+
+    </>
    
   )
 }
